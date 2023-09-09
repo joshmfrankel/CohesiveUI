c9174cdf2880a40d4e5948cdec68b6d8e38063bc
Josh Frankel <joshmfrankel@gmail.com>
2023-09-09 14:11:09 -0400

    Remove class_names in favor of built-in version

--- app/components/cohesive/application_component.rb
+++ app/components/cohesive/application_component.rb
@@ -10,22 +10,6 @@ module Cohesive
 
     private
 
-    # Ruby utility for ViewComponents to conditionally join CSS classes together
-    # inspired by https://github.com/JedWatson/classnames
-    def format_as_class_names(classes)
-      [].tap do |class_names|
-        if classes.is_a?(Array)
-          classes.each do |class_name|
-            class_names << format_as_class_names(class_name)
-          end
-        elsif classes.is_a?(Hash)
-          class_names << classes.select { |key| classes[key] == true }.keys
-        else
-          class_names << classes
-        end
-      end.join(" ")
-    end
-
     def post_initialize(*args, **keyword_args)
     end
   end
